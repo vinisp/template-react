60e1b90fad3fe30d9808a2c4487d6f854353795f
vinisp <aragao93.vinicius@gmail.com>
2022-02-07 18:07:00 -0300

    delete error

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import "./App.scss";
 import Nav from "./Components/Nav";
 import Home from "./Pages/Complete/Home";
-import { useState } from "react";
-
 
 function App() {
   return (
     <>
-        <div className="App">
-      
+      <div className="App">
         <Nav></Nav>
-        
+
         <Home />
-        
       </div>
-      
     </>
   );
 }
